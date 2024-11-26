d2d6057e4f938db58b406dffcbe68d8a84ae32bc
vineetha kondepudi <vineethakondepudi22@gmail.com>
2024-11-27 00:37:54 +0530

    main

--- src/routes/routing.tsx
+++ src/routes/routing.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import Home from '../components/home/home';
+import Home from '../home/home';
 import { Routes, Route } from 'react-router-dom';
-// import Layout from '../components/layout/layout';
-// import About from '../components/about/about';
-// import Contact from '../components/contact/contact';
-// import Skill from '../components/skills/skill';
-// import Project from '../components/project/project';
 
 
 export default function routing() {
   return (
    <Routes>
-     {/* <Route element={<Layout />}>
-    <Route path='/home' element={<Home/>}/>
-    <Route path='/About' element={<About/>}/>
-    <Route path='/projects' element={<Project/>}/>
-    <Route path='/skills' element={<Skill/>}/>
-    <Route path='/contact' element={<Contact />}/>
-    </Route> */}
       <Route path='' element={<Home/>}/>
    </Routes>
   )
